61b233df6500e577335fa1a44e26e16e0a664ef8
Abdul Aziz Karimov <abdulaziz.karimov.0708@gmil.com>
2024-04-24 11:28:04 +0500

    Second commit

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -11,34 +11,34 @@ import util.UploadUtil;
 public class MyTest extends BaseTest {
     private final ISettingsFile env = new JsonSettingsFile("config.json");
 
-	@Test
-    public void testCase1() {
-        BrowserUtil.goTo(env.getValue("/host_url").toString());
-
-        HomePage homePage = new HomePage();
-        Assert.assertTrue(homePage.state().isDisplayed(), "Home Page is not open");
-
-        homePage.clickOnHereLink();
-        FirstCard firstCard = new FirstCard();
-        Assert.assertTrue(firstCard.state().isDisplayed(), "First Card is not open");
-
-        firstCard.inputRandomValidPassword(RandomUtil.getValidPassword());
-        firstCard.inputRandomMail(RandomUtil.getRandomMail());
-        firstCard.inputRandomDomain(RandomUtil.getRandomDomain());
-        firstCard.acceptTermsAndConditions();
-        firstCard.clickOnNextBtn();
-
-        SecondCard secondCard = new SecondCard();
-        Assert.assertTrue(secondCard.state().isDisplayed(), "Second Card is not open");
-
-        secondCard.choose3randInterests();
-        secondCard.clickOnUploadLink();
-        UploadUtil.uploadImage(env.getValue("/image").toString());
-        secondCard.clickOnNextBtn();
-
-        ThirdCard thirdCard = new ThirdCard();
-        Assert.assertTrue(thirdCard.state().isDisplayed(), "Third Card is not open");
-    }
+//	@Test
+//    public void testCase1() {
+//        BrowserUtil.goTo(env.getValue("/host_url").toString());
+//
+//        HomePage homePage = new HomePage();
+//        Assert.assertTrue(homePage.state().isDisplayed(), "Home Page is not open");
+//
+//        homePage.clickOnHereLink();
+//        FirstCard firstCard = new FirstCard();
+//        Assert.assertTrue(firstCard.state().isDisplayed(), "First Card is not open");
+//
+//        firstCard.inputRandomValidPassword(RandomUtil.getValidPassword());
+//        firstCard.inputRandomMail(RandomUtil.getRandomMail());
+//        firstCard.inputRandomDomain(RandomUtil.getRandomDomain());
+//        firstCard.acceptTermsAndConditions();
+//        firstCard.clickOnNextBtn();
+//
+//        SecondCard secondCard = new SecondCard();
+//        Assert.assertTrue(secondCard.state().isDisplayed(), "Second Card is not open");
+//
+//        secondCard.choose3randInterests();
+//        secondCard.clickOnUploadLink();
+//        UploadUtil.uploadImage(env.getValue("/image").toString());
+//        secondCard.clickOnNextBtn();
+//
+//        ThirdCard thirdCard = new ThirdCard();
+//        Assert.assertTrue(thirdCard.state().isDisplayed(), "Third Card is not open");
+//    }
 
     @Test
     public void testCase2() {
